693035438639bf43fee51888a0efddda5becc57c
Hen Friedmann <99195954+friedmannhen@users.noreply.github.com>
2024-06-25 07:31:40 +0300

    remove netlify

--- app.js
+++ app.js
@@ -3,25 +3,13 @@ const express = require("express");
 const indexRoutes = require("./routes");
 const aboutRoutes = require("./routes/about");
 const projectRoutes = require("./routes/project");
-
-//for netlify
-const serverless = require("serverless-http");
-
 // Port
 const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-
-//for netlify
-const router = express.Router();
-app.use('/app/', router);
-module.exports.handler = serverless(app);
-
-// Set view engine to Pug
-
-// Serve static files
 app.use("/static", express.static("./public"));
 
 // Routes
@@ -35,7 +23,6 @@ app.use((req, res, next) => {
   err.status = 500;
   next(err);
 });
-
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status).render("error");
